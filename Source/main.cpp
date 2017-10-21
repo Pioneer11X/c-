598cddf77ff85168bb88b3c9317f5231ec805ac1
Sravan Karuturi <sravankumarkaruturi@gmail.com>
2017-10-21 14:41:33 -0400

    Made a Rectangle

--- Source/main.cpp
+++ Source/main.cpp
@@ -66,16 +66,28 @@ int main( )
         // Positions         // Colors
         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
         -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
-        0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top
+        -0.5f,  0.5f, 0.0f,   0.5f, 0.0f, 1.0f,   // Top Left
+		0.5f,  0.5f, 0.0f,   0.0f, 0.5f, 1.0f   // Top Right
     };
-    GLuint VBO, VAO;
+
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays( 1, &VAO );
     glGenBuffers( 1, &VBO );
+	glGenBuffers(1, &EBO);
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
     glBindVertexArray( VAO );
     
     glBindBuffer( GL_ARRAY_BUFFER, VBO );
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     
     // Position attribute
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( GLvoid * ) 0 );
@@ -100,7 +112,8 @@ int main( )
         // Draw the triangle
         ourShader.Use( );
         glBindVertexArray( VAO );
-        glDrawArrays( GL_TRIANGLES, 0, 3 );
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         // Swap the screen buffers
