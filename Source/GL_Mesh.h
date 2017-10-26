ac405ca36ec639135a5dabf4d35fd02275743c07
Sravan Karuturi <Sravan@Sravans-MBP.wireless.rit.edu>
2017-10-26 15:36:33 -0400

    Not working XCode

---
+++ Source/GL_Mesh.h
@@ -0,0 +1,19 @@
+//
+//  GL_Mesh.h
+//  Engine
+//
+//  Created by Sravan Karuturi on 10/26/17.
+//
+
+#ifndef GL_Mesh_h
+#define GL_Mesh_h
+
+#include "GenericMesh.h"
+
+class GL_Mesh : public GenericMesh {
+    
+    jkjbjhb
+    
+};
+
+#endif /* GL_Mesh_h */
---
+++ Source/GenericRenderer.h
@@ -0,0 +1,17 @@
+//
+//  GenericRenderer.h
+//  Engine
+//
+//  Created by Sravan Karuturi on 10/26/17.
+//
+
+#ifndef GenericRenderer_h
+#define GenericRenderer_h
+
+class GenericRenderer{
+  
+    
+    
+};
+
+#endif /* GenericRenderer_h */
